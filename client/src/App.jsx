e82d2a7dd5d8bab8d2cd4d1248030939d422f32b
Vedant Magarde <vedantmagarde@gmail.com>
2026-05-29 02:45:22 +0530

    store and slice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,27 @@ import Home from './pages/Home';
 import Auth from './pages/Auth';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { setUser } from './redux/userSlice.js';
 
 export const ServerUrl = "http://localhost:8000"
 
 function App() {
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const getUser = async () => {
 
       try {
         const result = await axios.get(ServerUrl + "/api/user/current-user", { withCredentials: true });
-        console.log(result.data);
+        // console.log(result.data);
+        dispatch(setUser(result.data));
       }
       catch (error) {
         console.log("Status:", error.response?.status);
         console.log("Data:", error.response?.data);
+        dispatch(setUser(null));
       }
 
     }
