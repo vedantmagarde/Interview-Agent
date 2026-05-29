e282277dec851e92218ef938c4305d0ebfde4327
Vedant Magarde <vedantmagarde@gmail.com>
2026-05-29 23:58:45 +0530

    home page building , have to resolve errors in functioning..

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { useSelector } from "react-redux";
+import { motion } from "motion/react";
+import { BsRobot, BsMic, BsClock, BsBarChart, BsFileEarmarkText } from "react-icons/bs";
+import { HiSparkles } from "react-icons/hi";
 
 function Home() {
+
+    const { userData } = useSelector((state) => state.user);
+
     return (
         <div className="min-h-screen bg-[#f3f3f3] flex flex-col">
             <Navbar />
+
+            <div className="flex-1 px-6 py-20">
+
+                <div className="flex justify-center mb-6">
+                    <div className="bg-gray-100 text-gray-600 text-sm px-4 py-2 rounded-full flex items-center gap-2">
+                        <HiSparkles size={16} className="bg-green50 text-green-600" />
+                        AI Powered Smart Interview Preparation Platform
+                    </div>
+
+                    <div className="text-center mb-28">
+                        <motion.h1
+                            className="text-4xl md:text-6xl font-semibold leading-tight max-w-4xl mx-auto">
+
+                        </motion.h1>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     )
 }
