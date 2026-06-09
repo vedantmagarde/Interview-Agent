0021abd7e0474dae94e5c54a2c6df195023a22cc
Vedant Magarde <vedantmagarde@gmail.com>
2026-06-10 02:18:16 +0530

    add videoSource , -speakText through utterance , -runIntro, -Speaking currentQuestion

--- client/src/components/Step2Interview.jsx
+++ client/src/components/Step2Interview.jsx
@@ -36,6 +36,159 @@ function Step2Interview({ interviewData, onFinish }) {
     const currentQuestion = questions[currentIndex];
 
 
+    useEffect(() => {
+        const loadVoices = () => {
+            // Text to voice....
+            const voices = window.speechSynthesis.getVoices();
+            if (!voices.length) return;
+
+            // Try known female voices first
+            const femaleVoice =
+                voices.find(v =>
+                    v.name.toLowerCase().includes("zira") ||
+                    v.name.toLowerCase().includes("samantha") ||
+                    v.name.toLowerCase().includes("female")
+                );
+
+            if (femaleVoice) {
+                setSelectedVoice(femaleVoice);
+                setVoiceGender("female");
+                return;
+            }
+
+            // Try known male voices
+            const maleVoice =
+                voices.find(v =>
+                    v.name.toLowerCase().includes("david") ||
+                    v.name.toLowerCase().includes("mark") ||
+                    v.name.toLowerCase().includes("male")
+                );
+
+            if (maleVoice) {
+                setSelectedVoice(maleVoice);
+                setVoiceGender("male");
+                return;
+            }
+
+            // Fallback: first voice (assume female)
+            setSelectedVoice(voices[0]);
+            setVoiceGender("female");
+        };
+
+        loadVoices();
+        window.speechSynthesis.onvoiceschanged = loadVoices;
+
+    }, [])
+
+
+    const videoSource = voiceGender === "male" ? maleVideo : femaleVideo;
+
+
+    /* ---------------- SPEAK FUNCTION ---------------- */
+    const speakText = (text) => {
+
+
+        return new Promise((resolve) => {
+            if (!window.speechSynthesis || !selectedVoice) {
+                resolve();
+                return;
+            }
+
+            window.speechSynthesis.cancel();
+
+            // Add natural pauses after commas and periods
+            const humanText = text
+                .replace(/,/g, ", ... ")
+                .replace(/\./g, ". ... ");
+
+            const utterance = new SpeechSynthesisUtterance(humanText);
+
+            utterance.voice = selectedVoice;
+
+            // Human-like pacing
+            utterance.rate = 0.92;     // slightly slower than normal
+            utterance.pitch = 1.05;    // small warmth
+            utterance.volume = 1;
+
+
+            utterance.onstart = () => {
+                setIsAIPlaying(true);
+                stopMic()
+                videoRef.current?.play();
+            };
+
+
+            utterance.onend = () => {
+                videoRef.current?.pause();
+                videoRef.current.currentTime = 0;
+                setIsAIPlaying(false);
+
+
+
+                if (isMicOn) {
+                    startMic();
+                }
+                setTimeout(() => {
+                    setSubtitle("");
+                    resolve();
+                }, 300);
+            };
+
+
+            setSubtitle(text);
+
+            window.speechSynthesis.speak(utterance);
+        });
+    };
+
+
+
+
+
+    useEffect(() => {
+        if (!selectedVoice) {
+            return;
+        }
+        const runIntro = async () => {
+            if (isIntroPhase) {
+                await speakText(
+                    `Hi ${userName}, it's great to meet you today. I hope you're feeling confident and ready.`
+                );
+
+                await speakText(
+                    "I'll ask you a few questions. Just answer naturally, and take your time. Let's begin."
+                );
+
+                setIsIntroPhase(false)
+
+            } else if (currentQuestion) {
+
+                await new Promise(r => setTimeout(r, 800));
+
+                // If last question (hard level)
+                if (currentIndex === questions.length - 1) {
+                    await speakText("Alright, this one might be a bit more challenging.");
+                }
+
+                // quesstion bolna shuru....
+                await speakText(currentQuestion.question);
+
+                if (isMicOn) {
+                    startMic();
+                }
+            }
+
+        }
+
+        runIntro();
+
+    }, [selectedVoice, isIntroPhase, currentIndex])
+
+
+
+
+
+
     return (
         <div className="min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6">
 
@@ -45,7 +198,9 @@ function Step2Interview({ interviewData, onFinish }) {
                 <div className='w-full lg:w-[35%] bg-white flex flex-col items-center p-6 space-y-6 border-r border-gray-200'>
                     <div className='w-full max-w-md rounded-2xl overflow-hidden shadow-xl'>
                         <video
-                            src={femaleVideo}
+                            src={videoSource}
+                            key={videoSource}
+                            ref={videoSource}
                             muted
                             playsInline
                             preload="auto"
@@ -55,6 +210,12 @@ function Step2Interview({ interviewData, onFinish }) {
 
 
                     {/* subtitle pending */}
+                    {subtitle && (
+                        <div className='w-full max-w-md bg-gray-50 border border-gray-200 rounded-xl p-4 shadow-sm'>
+                            <p className='text-gray-700 text-sm sm:text-base font-medium text-center leading-relaxed'>{subtitle}</p>
+                        </div>
+                    )}
+
 
 
                     {/* timer area */}
