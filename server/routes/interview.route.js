76572f35ea1e2c96dcb0f0324ace50929dc65585
Vedant Magarde <vedantmagarde@gmail.com>
2026-06-12 01:32:43 +0530

    add__  getMyInterviews --all interviews for given current user  ,  getInterviewReport --selected interview for the current user

--- server/routes/interview.route.js
+++ server/routes/interview.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import { analyzeResume, finishInterview, generateQuestion, submitAnswer } from "../controllers/interview.controller.js";
+import { analyzeResume, finishInterview, generateQuestion, getInterviewReport, getMyInterviews, submitAnswer } from "../controllers/interview.controller.js";
 import { upload } from "../middlewares/multer.js";
 
 
@@ -10,6 +10,10 @@ interviewRouter.post("/resume", isAuth, upload.single("resume"), analyzeResume);
 interviewRouter.post("/generate-questions", isAuth, generateQuestion);
 interviewRouter.post("/submit-answer", isAuth, submitAnswer);
 interviewRouter.post("/finish", isAuth, finishInterview);
+
+
+interviewRouter.get("/get-interview", isAuth, getMyInterviews);
+interviewRouter.get("/report/:id", isAuth, getInterviewReport);
 
 
 
